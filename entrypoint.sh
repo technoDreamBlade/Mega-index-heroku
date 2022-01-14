925a60e13b1ba12e8697205c1686718a22a6f342
technoDreamBlade <97745372+technoDreamBlade@users.noreply.github.com>
2022-01-14 20:14:25 +0530

    Use Drive

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rclone version
-rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
+wget $UserName >> /.config/rclone/rclone.conf
 rclone version
-rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
+rclone serve http gd_qbit: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
 
 
